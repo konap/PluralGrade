3f741f66cc3ff048970294c2c3d24771b51e9041
konap <ko.9898@yandex.ua>
2018-10-04 03:05:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Scott Grade Book";
+            Console.Write(g2.Name);
+
+            
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Всем привет");
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(81.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatictics();
+            Console.WriteLine(stats.AverageGrade);
+            
+        }
+    }
+}
